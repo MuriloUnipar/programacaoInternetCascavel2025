06547e50dcfe01fd981f48a67f9ec5c649ff106d
MuriloUnipar <murilo91003@edu.unipar.br>
2025-05-05 22:22:29 -0300

    aula js 2

---
+++ atividadesJS/at2/script.js
@@ -0,0 +1,15 @@
+const btTroco = document.querySelector("button")
+
+btTroco.addEventListener("click", () => {
+  const inputA = document.querySelector(".valueA")
+  const inputB = document.querySelector(".valueB")
+
+  let result = Number(inputA.value) - Number(inputB.value)  
+
+  if (isNaN(result)) {
+    result = "Valor Digitado Inválido!"
+  } 
+
+  const spanResult = document.querySelector("span")
+  spanResult.textContent = result
+})
